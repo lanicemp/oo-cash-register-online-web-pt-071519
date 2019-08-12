6ece72204776ffacb15d44ea48523bba4ec131b4
LaNice Powell <lanicemp@aol.com>
2019-08-12 03:42:50 +0000

    Automatically backed up by Learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -39,7 +39,7 @@ class CashRegister
   end
   
   def void_last_transaction
-    # self.total = @total - @transactions.pop 
+     self.total = @total - @transactions.pop 
    # else
      binding.pry 
     self.total = @total - @transactions.all
